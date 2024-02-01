01d8f43c1feed33ea525849a42f81ea95f4cbb03
Tyler M. Neher <tyler@neherdata.com>
2024-02-01 18:05:33 -0500

    init

---
+++ NDS Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NDS Map
+//
+//  Created by Tyler M. Neher on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NDS Map/NDS_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDS_MapApp.swift
+//  NDS Map
+//
+//  Created by Tyler M. Neher on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NDS_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
